855416234eff3f401f4a903f8a4ed84a2ba28537
David Dinh <daviddinh93@gmail.com>
2016-09-08 15:14:19 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+    render html: "¡hola, mundo!"
+  end
+
+  
+def goodbye
+    render html: "goodbye, mundo goes where he pleases!"
+  end
+  
+end
+
